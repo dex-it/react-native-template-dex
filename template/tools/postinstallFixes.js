ee498cbd548e727d26ec189a0c480bb106612cb2
svbutko <svbutko@hotmail.com>
2019-10-08 22:45:36 +0300

    Fix the packager's starting issue related to Node 12.11 changes

--- template/tools/postinstallFixes.js
+++ template/tools/postinstallFixes.js
@@ -2,11 +2,36 @@ const dir = __dirname;
 const path = require("path");
 const replaceInFile = require("./replaceInFile");
 
+replaceMetroBlackListForNode();
 preventBundleInDebugForIOS();
 preventRunReactPackagerIOS();
 
+function replaceMetroBlackListForNode() {
+    console.log("Replace metro blacklist for node >= 12.11.x");
+
+    if (process.version.startsWith("v12.1")) {
+        const file = path.join(dir, "..", "node_modules", "metro-config", "src", "defaults", "blacklist.js");
+
+        replaceInFile(file,
+            "var sharedBlacklist = [\n" +
+            "  /node_modules[/\\\\]react[/\\\\]dist[/\\\\].*/,\n" +
+            "  /website\\/node_modules\\/.*/,\n" +
+            "  /heapCapture\\/bundle\\.js/,\n" +
+            "  /.*\\/__tests__\\/.*/\n" +
+            "];",
+            "var sharedBlacklist = [\n" +
+            "  /node_modules[\\/\\\\]react[\\/\\\\]dist[\\/\\\\].*/,\n" +
+            "  /website\\/node_modules\\/.*/,\n" +
+            "  /heapCapture\\/bundle\\.js/,\n" +
+            "  /.*\\/__tests__\\/.*/\n" +
+            "];"
+        );
+    }
+    console.log("Process version: ", process.version);
+}
+
 function preventRunReactPackagerIOS() {
-    console.log("preventRunReactPackagerIOS");
+    console.log("Prevent react packager from running on iOS");
     const file = path.join(dir, "..", "node_modules", "react-native", "React", "React.xcodeproj", "project.pbxproj");
 
     replaceInFile(file, [
@@ -16,7 +41,7 @@ function preventRunReactPackagerIOS() {
 }
 
 function preventBundleInDebugForIOS() {
-    console.log("preventBundleInDebugForIOS");
+    console.log("Prevent bundling in debug on iOS");
     const file = path.join(dir, "..", "node_modules", "react-native", "scripts", "react-native-xcode.sh");
 
     replaceInFile(file, /"\$PLATFORM_NAME" == \*simulator/g, "\"\$PLATFORM_NAME\"")
